d9a0ff4a4193663f5428adc4af1c4024c7872311
Ayushman Praharaj <ayushmanpraharaj1@gmail.com>
2026-04-01 20:22:47 +0530

    Landing page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import UserLogin from './pages/UserLogin';
+import UserSignup from './pages/UserSignup';
+import CaptainLogin from './pages/CaptainLogin';
+import CpatinSignup from './pages/CpatinSignup';
 const App = () => {
   return (
-    <div className='bg-red-700'>
-      App
+    <div>
+      <Routes>
+        <Route path ='/' element ={<Home/>}/>
+        <Route path ='/login' element ={<UserLogin/>}/>
+        <Route path ='/signup' element ={<UserSignup/>}/>
+        <Route path ='/capatin-login' element ={<CaptainLogin/>}/>
+        <Route path ='/captain-signup' element ={<CpatinSignup/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import bgimage from '../assets/bg.png'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div
+      className="bg-cover bg-bottom  bg-center h-screen pt-8 flex justify-between flex-col w-full"
+      style={{ backgroundImage: `url(${bgimage})` }}
+    >
+      <img className="w-36 ml-0" src="/finalLogo.png" alt="Drivigo" />
+
+      <div className="bg-white py-4 pb-10 px-4">
+        <h2 className="text-3xl font-bold">
+          Get Started with Drivigo
+        </h2>
+
+        <Link to='/login' className="flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5">
+          Continue
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
