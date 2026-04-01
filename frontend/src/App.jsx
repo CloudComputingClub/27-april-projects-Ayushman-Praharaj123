8e3bc8f7f877c55e5b3d3b5e69d3962799b70644
Ayushman Praharaj <ayushmanpraharaj1@gmail.com>
2026-04-01 19:09:18 +0530

    Intialization of of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-red-700'>
+      App
     </div>
   )
 }
 
-export default App
+export default App;
