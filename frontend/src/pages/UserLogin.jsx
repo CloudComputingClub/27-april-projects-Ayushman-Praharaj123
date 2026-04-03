5185aced07df4d24616a60f1e33eb83649b452ea
Ayushman Praharaj <ayushmanpraharaj1@gmail.com>
2026-04-03 20:56:45 +0530

    ggg

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-
-
 const UserLogin = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
